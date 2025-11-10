12eccd93a3cef2a9dc4352042519179dd5531ddb
Sameer-Abdul <abdulsameer8787@gmail.com>
2025-11-10 14:39:38 +0530

    updated 39

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-  safelist: [
-    'bg-linear-to-r',
-    'from-gray-50',
-    'to-gray-100'
-  ]
-}
+};
